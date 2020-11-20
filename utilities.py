e284d8bd70b9f36af9a65b7b0f4120702e9ff564
guangyuzhang <13621160019@163.com>
2020-11-20 17:37:00 +0800

    implemented space statistics report

--- utilities.py
+++ utilities.py
@@ -3,6 +3,7 @@ import mysql.connector
 import collections
 from decimal import *
 import config
+import statistics
 
 
 ########################################################################################################################
@@ -132,7 +133,6 @@ def aggregate_hourly_data_by_period(rows_hourly, start_datetime_utc, end_datetim
         return result_rows_monthly
 
     elif period_type == "yearly":
-        print(start_datetime_utc)
         result_rows_yearly = list()
         # todo: add config.working_day_start_time_local
         # todo: add config.minutes_to_count
@@ -457,10 +457,6 @@ def averaging_hourly_data_by_period(rows_hourly, start_datetime_utc, end_datetim
                 maximum = sub_maximum
             current_datetime_utc += timedelta(days=1)
 
-        average = total / counter if counter > 0 else None
-        print(average)
-        print(maximum)
-        print(result_rows_daily)
         return result_rows_daily, average, maximum
 
     elif period_type == "monthly":
@@ -564,7 +560,6 @@ def averaging_hourly_data_by_period(rows_hourly, start_datetime_utc, end_datetim
         return result_rows_monthly, average, maximum
 
     elif period_type == "yearly":
-        print(start_datetime_utc)
         result_rows_yearly = list()
         # todo: add config.working_day_start_time_local
         # todo: add config.minutes_to_count
@@ -613,3 +608,266 @@ def averaging_hourly_data_by_period(rows_hourly, start_datetime_utc, end_datetim
 
         average = total / counter if counter > 0 else None
         return result_rows_yearly, average, maximum
+
+
+########################################################################################################################
+# Statistics calculator of hourly data by period
+#   rows_hourly: list of (start_datetime_utc, actual_value), should belong to one energy_category_id
+#   start_datetime_utc: start datetime in utc
+#   end_datetime_utc: end datetime in utc
+#   period_type: one of the following period types, 'hourly', 'daily', 'monthly' and 'yearly'
+# Returns: periodically data of values and statistics of mean, median, minimum, maximum, stdev and variance
+# Note: this procedure doesn't work with multiple energy categories
+########################################################################################################################
+def statistics_hourly_data_by_period(rows_hourly, start_datetime_utc, end_datetime_utc, period_type):
+    # todo: validate parameters
+    start_datetime_utc = start_datetime_utc.replace(tzinfo=None)
+    end_datetime_utc = end_datetime_utc.replace(tzinfo=None)
+
+    if period_type == "hourly":
+        result_rows_hourly = list()
+        sample_data = list()
+        # todo: add config.working_day_start_time_local
+        # todo: add config.minutes_to_count
+        counter = 0
+        mean = None
+        median = None
+        minimum = None
+        maximum = None
+        stdev = None
+        variance = None
+        current_datetime_utc = start_datetime_utc.replace(minute=0, second=0, microsecond=0, tzinfo=None)
+        while current_datetime_utc <= end_datetime_utc:
+            sub_total = Decimal(0.0)
+            for row in rows_hourly:
+                if current_datetime_utc <= row[0] < current_datetime_utc + \
+                        timedelta(minutes=config.minutes_to_count):
+                    sub_total += row[1]
+
+            result_rows_hourly.append((current_datetime_utc, sub_total))
+            sample_data.append(sub_total)
+
+            counter += 1
+            if minimum is None:
+                minimum = sub_total
+            elif minimum > sub_total:
+                minimum = sub_total
+
+            if maximum is None:
+                maximum = sub_total
+            elif maximum < sub_total:
+                maximum = sub_total
+
+            current_datetime_utc += timedelta(minutes=config.minutes_to_count)
+
+        if len(sample_data) > 1:
+            mean = statistics.mean(sample_data)
+            median = statistics.median(sample_data)
+            stdev = statistics.stdev(sample_data)
+            variance = statistics.variance(sample_data)
+
+        return result_rows_hourly, mean, median, minimum, maximum, stdev, variance
+
+    elif period_type == "daily":
+        result_rows_daily = list()
+        sample_data = list()
+        # todo: add config.working_day_start_time_local
+        # todo: add config.minutes_to_count
+        counter = 0
+        mean = None
+        median = None
+        minimum = None
+        maximum = None
+        stdev = None
+        variance = None
+        # calculate the start datetime in utc of the first day in local
+        start_datetime_local = start_datetime_utc + timedelta(hours=int(config.utc_offset[1:3]))
+        current_datetime_utc = start_datetime_local.replace(hour=0) - timedelta(hours=int(config.utc_offset[1:3]))
+        while current_datetime_utc <= end_datetime_utc:
+            sub_total = Decimal(0.0)
+            for row in rows_hourly:
+                if current_datetime_utc <= row[0] < current_datetime_utc + timedelta(days=1):
+                    sub_total += row[1]
+
+            result_rows_daily.append((current_datetime_utc, sub_total))
+            sample_data.append(sub_total)
+
+            counter += 1
+            if minimum is None:
+                minimum = sub_total
+            elif minimum > sub_total:
+                minimum = sub_total
+
+            if maximum is None:
+                maximum = sub_total
+            elif maximum < sub_total:
+                maximum = sub_total
+            current_datetime_utc += timedelta(days=1)
+
+        if len(sample_data) > 1:
+            mean = statistics.mean(sample_data)
+            median = statistics.median(sample_data)
+            stdev = statistics.stdev(sample_data)
+            variance = statistics.variance(sample_data)
+
+        return result_rows_daily, mean, median, minimum, maximum, stdev, variance
+
+    elif period_type == "monthly":
+        result_rows_monthly = list()
+        sample_data = list()
+        # todo: add config.working_day_start_time_local
+        # todo: add config.minutes_to_count
+        counter = 0
+        mean = None
+        median = None
+        minimum = None
+        maximum = None
+        stdev = None
+        variance = None
+        # calculate the start datetime in utc of the first day in the first month in local
+        start_datetime_local = start_datetime_utc + timedelta(hours=int(config.utc_offset[1:3]))
+        current_datetime_utc = start_datetime_local.replace(day=1, hour=0) - timedelta(hours=int(config.utc_offset[1:3]))
+
+        while current_datetime_utc <= end_datetime_utc:
+            # calculate the next datetime in utc
+            if current_datetime_utc.month == 1:
+                temp_day = 28
+                ny = current_datetime_utc.year
+                if (ny % 100 != 0 and ny % 4 == 0) or (ny % 100 == 0 and ny % 400 == 0):
+                    temp_day = 29
+
+                next_datetime_utc = datetime(year=current_datetime_utc.year,
+                                             month=current_datetime_utc.month + 1,
+                                             day=temp_day,
+                                             hour=current_datetime_utc.hour,
+                                             minute=current_datetime_utc.minute,
+                                             second=0,
+                                             microsecond=0,
+                                             tzinfo=None)
+            elif current_datetime_utc.month == 2:
+                next_datetime_utc = datetime(year=current_datetime_utc.year,
+                                             month=current_datetime_utc.month + 1,
+                                             day=31,
+                                             hour=current_datetime_utc.hour,
+                                             minute=current_datetime_utc.minute,
+                                             second=0,
+                                             microsecond=0,
+                                             tzinfo=None)
+            elif current_datetime_utc.month in [3, 5, 8, 10]:
+                next_datetime_utc = datetime(year=current_datetime_utc.year,
+                                             month=current_datetime_utc.month + 1,
+                                             day=30,
+                                             hour=current_datetime_utc.hour,
+                                             minute=current_datetime_utc.minute,
+                                             second=0,
+                                             microsecond=0,
+                                             tzinfo=None)
+            elif current_datetime_utc.month == 7:
+                next_datetime_utc = datetime(year=current_datetime_utc.year,
+                                             month=current_datetime_utc.month + 1,
+                                             day=31,
+                                             hour=current_datetime_utc.hour,
+                                             minute=current_datetime_utc.minute,
+                                             second=0,
+                                             microsecond=0,
+                                             tzinfo=None)
+            elif current_datetime_utc.month in [4, 6, 9, 11]:
+                next_datetime_utc = datetime(year=current_datetime_utc.year,
+                                             month=current_datetime_utc.month + 1,
+                                             day=31,
+                                             hour=current_datetime_utc.hour,
+                                             minute=current_datetime_utc.minute,
+                                             second=0,
+                                             microsecond=0,
+                                             tzinfo=None)
+            elif current_datetime_utc.month == 12:
+                next_datetime_utc = datetime(year=current_datetime_utc.year + 1,
+                                             month=1,
+                                             day=31,
+                                             hour=current_datetime_utc.hour,
+                                             minute=current_datetime_utc.minute,
+                                             second=0,
+                                             microsecond=0,
+                                             tzinfo=None)
+
+            sub_total = Decimal(0.0)
+            for row in rows_hourly:
+                if current_datetime_utc <= row[0] < next_datetime_utc:
+                    sub_total += row[1]
+
+            result_rows_monthly.append((current_datetime_utc, sub_total))
+            sample_data.append(sub_total)
+
+            counter += 1
+            if minimum is None:
+                minimum = sub_total
+            elif minimum > sub_total:
+                minimum = sub_total
+
+            if maximum is None:
+                maximum = sub_total
+            elif maximum < sub_total:
+                maximum = sub_total
+            current_datetime_utc = next_datetime_utc
+
+        if len(sample_data) > 1:
+            mean = statistics.mean(sample_data)
+            median = statistics.median(sample_data)
+            stdev = statistics.stdev(sample_data)
+            variance = statistics.variance(sample_data)
+
+        return result_rows_monthly, mean, median, minimum, maximum, stdev, variance
+
+    elif period_type == "yearly":
+        result_rows_yearly = list()
+        sample_data = list()
+        # todo: add config.working_day_start_time_local
+        # todo: add config.minutes_to_count
+        mean = None
+        median = None
+        minimum = None
+        maximum = None
+        stdev = None
+        variance = None
+        # calculate the start datetime in utc of the first day in the first month in local
+        start_datetime_local = start_datetime_utc + timedelta(hours=int(config.utc_offset[1:3]))
+        current_datetime_utc = start_datetime_local.replace(month=1, day=1, hour=0) - timedelta(
+            hours=int(config.utc_offset[1:3]))
+
+        while current_datetime_utc <= end_datetime_utc:
+            # calculate the next datetime in utc
+            # todo: timedelta of year
+            next_datetime_utc = datetime(year=current_datetime_utc.year + 2,
+                                         month=1,
+                                         day=1,
+                                         hour=current_datetime_utc.hour,
+                                         minute=current_datetime_utc.minute,
+                                         second=current_datetime_utc.second,
+                                         microsecond=current_datetime_utc.microsecond,
+                                         tzinfo=current_datetime_utc.tzinfo) - timedelta(days=1)
+            sub_total = Decimal(0.0)
+            for row in rows_hourly:
+                if current_datetime_utc <= row[0] < next_datetime_utc:
+                    sub_total += row[1]
+
+            result_rows_yearly.append((current_datetime_utc, sub_total))
+            sample_data.append(sub_total)
+
+            if minimum is None:
+                minimum = sub_total
+            elif minimum > sub_total:
+                minimum = sub_total
+            if maximum is None:
+                maximum = sub_total
+            elif maximum < sub_total:
+                maximum = sub_total
+
+            current_datetime_utc = next_datetime_utc
+
+        if len(sample_data) > 1:
+            mean = statistics.mean(sample_data)
+            median = statistics.median(sample_data)
+            stdev = statistics.stdev(sample_data)
+            variance = statistics.variance(sample_data)
+
+        return result_rows_yearly, mean, median, minimum, maximum, stdev, variance
