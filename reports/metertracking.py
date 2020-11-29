7296d24bc1a1d0ffc7b35bfbfe665998314e503c
guangyuzhang <13621160019@163.com>
2020-11-29 08:46:16 +0800

    updated meter tracking report to get all meters in the space tree

--- reports/metertracking.py
+++ reports/metertracking.py
@@ -2,9 +2,7 @@ import falcon
 import simplejson as json
 import mysql.connector
 import config
-from datetime import datetime, timedelta, timezone
-import utilities
-from decimal import *
+from anytree import Node, AnyNode, LevelOrderIter
 
 
 class Reporting:
@@ -19,8 +17,9 @@ class Reporting:
     ####################################################################################################################
     # PROCEDURES
     # Step 1: valid parameters
-    # Step 2: query the meters
-    # Step 3: construct the report
+    # Step 2: build a space tree
+    # Step 3: query all meters in the space tree
+    # Step 4: construct the report
     ####################################################################################################################
     @staticmethod
     def on_get(req, resp):
@@ -36,21 +35,53 @@ class Reporting:
             space_id = str.strip(space_id)
             if not space_id.isdigit() or int(space_id) <= 0:
                 raise falcon.HTTPError(falcon.HTTP_400, title='API.BAD_REQUEST', description='API.INVALID_SPACE_ID')
+            else:
+                space_id = int(space_id)
 
-        ################################################################################################################
-        # Step 2: query the meters
-        ################################################################################################################
         cnx = mysql.connector.connect(**config.myems_system_db)
         cursor = cnx.cursor(dictionary=True)
+
+        cursor.execute(" SELECT name "
+                       " FROM tbl_spaces "
+                       " WHERE id = %s ", (space_id,))
+        if cursor.fetchone() is None:
+            cursor.close()
+            cnx.disconnect()
+            raise falcon.HTTPError(falcon.HTTP_404, title='API.NOT_FOUND',
+                                   description='API.SPACE_NOT_FOUND')
+
+        ################################################################################################################
+        # Step 2: build a space tree
+        ################################################################################################################
+
+        query = (" SELECT id, name, parent_space_id "
+                 " FROM tbl_spaces "
+                 " ORDER BY id ")
+        cursor.execute(query)
+        rows_spaces = cursor.fetchall()
+        node_dict = dict()
+        if rows_spaces is not None and len(rows_spaces) > 0:
+            for row in rows_spaces:
+                parent_node = node_dict[row['parent_space_id']] if row['parent_space_id'] is not None else None
+                node_dict[row['id']] = AnyNode(id=row['id'], parent=parent_node, name=row['name'])
+        print(node_dict)
+        ################################################################################################################
+        # Step 3: query all meters in the space tree
+        ################################################################################################################
         meter_list = list()
+        sub_space_dict = dict()
+
+        for node in LevelOrderIter(node_dict[space_id]):
+            sub_space_dict[node.id] = node.name
 
         cursor.execute(" SELECT m.id, m.name AS meter_name, s.name AS space_name, "
                        "        cc.name AS cost_center_name, ec.name AS energy_category_name, "
                        "         m.description "
                        " FROM tbl_spaces s, tbl_spaces_meters sm, tbl_meters m, tbl_cost_centers cc, "
                        "      tbl_energy_categories ec "
-                       " WHERE s.id = %s AND sm.space_id = s.id AND sm.meter_id = m.id "
-                       "       AND m.cost_center_id = cc.id AND m.energy_category_id = ec.id ", (space_id,))
+                       " WHERE s.id IN ( " + ', '.join(map(str, sub_space_dict.keys())) + ") "
+                       "       AND sm.space_id = s.id AND sm.meter_id = m.id "
+                       "       AND m.cost_center_id = cc.id AND m.energy_category_id = ec.id ", )
         rows_meters = cursor.fetchall()
         if rows_meters is not None and len(rows_meters) > 0:
             for row in rows_meters:
@@ -67,7 +98,7 @@ class Reporting:
             cnx.disconnect()
 
         ################################################################################################################
-        # Step 5: construct the report
+        # Step 4: construct the report
         ################################################################################################################
 
         resp.body = json.dumps(meter_list)
