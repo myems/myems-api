b409cb450337c8ca53e054e97e7f59ced0b6bcd9
YangZhang-GitHub <2533471770@qq.com>
2021-02-25 00:00:51 +0800

    Improved the Execl Exporter

--- excelexporters/tenantenergyitem.py
+++ excelexporters/tenantenergyitem.py
@@ -3,6 +3,7 @@ import uuid
 import os
 from openpyxl.chart import (
     PieChart,
+    LineChart,
     BarChart,
     Reference,
 )
@@ -72,7 +73,7 @@ def generate_excel(report,
     # Row height
     ws.row_dimensions[1].height = 102
     for i in range(2, 2000 + 1):
-        ws.row_dimensions[i].height = 30
+        ws.row_dimensions[i].height = 42
 
     # Col width
     ws.column_dimensions['A'].width = 1.5
@@ -84,9 +85,7 @@ def generate_excel(report,
 
     # Font
     name_font = Font(name='Constantia', size=15, bold=True)
-    name_small_font = Font(name='Constantia', size=10, bold=True)
     title_font = Font(name='宋体', size=15, bold=True)
-    title_small_font = Font(name='宋体', size=10, bold=True)
     data_font = Font(name='Franklin Gothic Book', size=11)
 
     table_fill = PatternFill(fill_type='solid', fgColor='1F497D')
@@ -102,25 +101,25 @@ def generate_excel(report,
     b_c_alignment = Alignment(vertical='bottom',
                               horizontal='center',
                               text_rotation=0,
-                              wrap_text=False,
+                              wrap_text=True,
                               shrink_to_fit=False,
                               indent=0)
     c_c_alignment = Alignment(vertical='center',
                               horizontal='center',
                               text_rotation=0,
-                              wrap_text=False,
+                              wrap_text=True,
                               shrink_to_fit=False,
                               indent=0)
     b_r_alignment = Alignment(vertical='bottom',
                               horizontal='right',
                               text_rotation=0,
-                              wrap_text=False,
+                              wrap_text=True,
                               shrink_to_fit=False,
                               indent=0)
     c_r_alignment = Alignment(vertical='bottom',
                               horizontal='center',
                               text_rotation=0,
-                              wrap_text=False,
+                              wrap_text=True,
                               shrink_to_fit=False,
                               indent=0)
     # Img
@@ -131,11 +130,32 @@ def generate_excel(report,
     ws.add_image(img, 'B1')
 
     # Title
-    ws.merge_cells('B3:I3')
+    ws.row_dimensions[3].height = 60
+
     ws['B3'].font = name_font
-    ws['B3'].alignment = b_c_alignment
-    ws['B3'] = 'Name: ' + name + '     Period: ' + period_type + \
-               '     Date: ' + reporting_start_datetime_local + "__" + reporting_end_datetime_local
+    ws['B3'].alignment = b_r_alignment
+    ws['B3'] = 'Name:'
+    ws['C3'].border = b_border
+    ws['C3'].alignment = b_c_alignment
+    ws['C3'].font = name_font
+    ws['C3'] = name
+
+    ws['D3'].font = name_font
+    ws['D3'].alignment = b_r_alignment
+    ws['D3'] = 'Period:'
+    ws['E3'].border = b_border
+    ws['E3'].alignment = b_c_alignment
+    ws['E3'].font = name_font
+    ws['E3'] = period_type
+
+    ws['F3'].font = name_font
+    ws['F3'].alignment = b_r_alignment
+    ws['F3'] = 'Date:'
+    ws['G3'].border = b_border
+    ws['G3'].alignment = b_c_alignment
+    ws['G3'].font = name_font
+    ws['G3'] = reporting_start_datetime_local + "__" + reporting_end_datetime_local
+    ws.merge_cells("G3:H3")
 
     if "reporting_period" not in report.keys() or \
             "names" not in report['reporting_period'].keys() or len(report['reporting_period']['names']) == 0:
@@ -166,13 +186,14 @@ def generate_excel(report,
         category = reporting_period_data['names']
         ca_len = len(category)
 
+        ws.row_dimensions[current_row_number].height = 60
         ws['B' + str(current_row_number)].fill = table_fill
 
         col = 'C'
 
         for i in range(0, ca_len):
             ws[col + str(current_row_number)].fill = table_fill
-            ws[col + str(current_row_number)].font = name_small_font
+            ws[col + str(current_row_number)].font = name_font
             ws[col + str(current_row_number)].alignment = c_c_alignment
             ws[col + str(current_row_number)].border = f_border
             ws[col + str(current_row_number)] = \
@@ -250,7 +271,7 @@ def generate_excel(report,
 
             ws['B' + str(current_row_number)].fill = table_fill
 
-            ws['C' + str(current_row_number)].font = name_small_font
+            ws['C' + str(current_row_number)].font = name_font
             ws['C' + str(current_row_number)].fill = table_fill
             ws['C' + str(current_row_number)].alignment = c_c_alignment
             ws['C' + str(current_row_number)].border = f_border
@@ -281,7 +302,7 @@ def generate_excel(report,
             pie_data = Reference(ws, min_col=3, min_row=table_start_row_number, max_row=table_end_row_number)
             pie.add_data(pie_data, titles_from_data=True)
             pie.set_categories(labels)
-            pie.height = 5.25
+            pie.height = 6.6
             pie.width = 9
             s1 = pie.series[0]
             s1.dLbls = DataLabelList()
@@ -325,8 +346,9 @@ def generate_excel(report,
             current_row_number += ca_len * 6
             table_start_row_number = current_row_number
 
+            ws.row_dimensions[current_row_number].height = 60
             ws['B' + str(current_row_number)].fill = table_fill
-            ws['B' + str(current_row_number)].font = title_small_font
+            ws['B' + str(current_row_number)].font = title_font
             ws['B' + str(current_row_number)].alignment = c_c_alignment
             ws['B' + str(current_row_number)].border = f_border
             ws['B' + str(current_row_number)] = '日期时间'
@@ -335,7 +357,7 @@ def generate_excel(report,
 
             for i in range(0, ca_len):
                 ws[col + str(current_row_number)].fill = table_fill
-                ws[col + str(current_row_number)].font = title_small_font
+                ws[col + str(current_row_number)].font = title_font
                 ws[col + str(current_row_number)].alignment = c_c_alignment
                 ws[col + str(current_row_number)].border = f_border
                 ws[col + str(current_row_number)] = \
@@ -401,24 +423,29 @@ def generate_excel(report,
                 min_width_number = 11.5
 
             for i in range(0, ca_len):
-                bar = BarChart()
-                bar.title = \
+                line = LineChart()
+                line.title = '报告期消耗 - ' + \
                     reporting_period_data['names'][i] + " (" + reporting_period_data['units'][i] + ")"
                 labels = Reference(ws, min_col=2, min_row=table_start_row_number + 1, max_row=table_end_row_number)
-                bar_data = Reference(ws, min_col=3 + i, min_row=table_start_row_number, max_row=table_end_row_number)
-                bar.add_data(bar_data, titles_from_data=True)
-                bar.set_categories(labels)
-                bar.height = 5.85
-                bar.width = format_time_width_number * len(time) if len(time) > min_len_number else min_width_number
-                if bar.width > 24:
-                    bar.width = 24
-                bar.dLbls = DataLabelList()
-                bar.dLbls.showVal = False
-                bar.dLbls.showPercent = True
+                line_data = Reference(ws, min_col=3 + i, min_row=table_start_row_number, max_row=table_end_row_number)
+                line.add_data(line_data, titles_from_data=True)
+                line.set_categories(labels)
+                line_data = line.series[0]
+                line_data.marker.symbol = "circle"
+                line_data.smooth = True
+                line.x_axis.crosses = 'min'
+                line.height = 8.25
+                line.width = format_time_width_number * len(time) if len(time) > min_len_number else min_width_number
+                if line.width > 24:
+                    line.width = 24
+                line.dLbls = DataLabelList()
+                line.dLbls.dLblPos = 't'
+                line.dLbls.showVal = True
+                line.dLbls.showPercent = False
                 chart_col = 'B'
                 chart_cell = chart_col + str(chart_start_row_number)
                 chart_start_row_number += 6
-                ws.add_chart(bar, chart_cell)
+                ws.add_chart(line, chart_cell)
 
     filename = str(uuid.uuid4()) + '.xlsx'
     wb.save(filename)
